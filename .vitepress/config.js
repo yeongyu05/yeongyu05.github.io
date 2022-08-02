235488008912f4c5cf58ccd6b98558cfeca3790b
yeongyu05 <89061456+yeongyu05@users.noreply.github.com>
2022-08-02 19:57:42 +0900

    init

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,72 +6,68 @@ const mdDir = path.resolve(rootDir, "docs");
 
 export default {
     title: "연규의 blog",
-    description:"A VitePress site",
-    srcDir : mdDir,
+    description: "A VitePress site",
+    srcDir: mdDir,
     themeConfig: {
-        sidebar:getSidebar(),
+        sidebar: getSidebar(),
         nav: getNav()
     },
 
 }
 
-function getSidebar(){
+function getSidebar() {
     return {
         "/": [
             {
                 text: 'TIL📖',
                 collapsible: true,
                 items: [
-                    { text: '2022년07월', link: '/TIL/202207/202207'},
-                    { text: '2022년07월18일', link: '/TIL/202207/20220718'},
-                    { text: '2022년07월19일', link: '/TIL/202207/20220719'},
-                    { text: '2022년07월20일', link: '/TIL/202207/20220720'},
-                    { text: '2022년07월21일', link: '/TIL/202207/20220721'},
-                    { text: '2022년07월22일', link: '/TIL/202207/20220722'},
-                    { text: '2022년07월23일', link: '/TIL/202207/20220723'},
-                    { text: '2022년07월24일', link: '/TIL/202207/20220724'},
-                    { text: '2022년07월25일', link: '/TIL/202207/20220725'},
-                    { text: '2022년07월26일', link: '/TIL/202207/20220726'},
-                    { text: '2022년07월27일', link: '/TIL/202207/20220727'},
-                    { text: '2022년07월28일', link: '/TIL/202207/20220728'},
-                    { text: '2022년07월29일', link: '/TIL/202207/20220729'},
-                    { text: '2022년07월30일', link: '/TIL/202207/20220730'},
-                    { text: '2022년07월31일', link: '/TIL/202207/20220731'},
-                ]
-            },
-            {
-                text: 'TIL📖',
-                collapsible: true,
+                    { text: '2022년07월', link: '/TIL/202207/202207' },
+                    { text: '2022년07월18일', link: '/TIL/202207/20220718' },
+                    { text: '2022년07월19일', link: '/TIL/202207/20220719' },
+                    { text: '2022년07월20일', link: '/TIL/202207/20220720' },
+                    { text: '2022년07월21일', link: '/TIL/202207/20220721' },
+                    { text: '2022년07월22일', link: '/TIL/202207/20220722' },
+                    { text: '2022년07월23일', link: '/TIL/202207/20220723' },
+                    { text: '2022년07월24일', link: '/TIL/202207/20220724' },
+                    { text: '2022년07월25일', link: '/TIL/202207/20220725' },
+                    { text: '2022년07월26일', link: '/TIL/202207/20220726' },
+                    { text: '2022년07월27일', link: '/TIL/202207/20220727' },
+                    { text: '2022년07월28일', link: '/TIL/202207/20220728' },
+                    { text: '2022년07월29일', link: '/TIL/202207/20220729' },
+                    { text: '2022년07월30일', link: '/TIL/202207/20220730' },
+                    { text: '2022년07월31일', link: '/TIL/202207/20220731' },
+                ],
                 items: [
-                    { text: '2022년08월', link: '/TIL/202208/202208'},
-                    { text: '2022년08월01일', link: '/TIL/202207/20220801'},
+                    { text: '2022년08월', link: '/TIL/202208/202208' },
+                    { text: '2022년08월01일', link: '/TIL/202207/20220801' },
                 ]
-            },
-        ],
+            }
+        ]
     }
 }
 
-function getNav(){
+function getNav() {
     return [
         {
             text: 'Menu',
             items: [
-              {
-                  text:'TIL',
-                  link:"/TIL/202207/202207",
-                  activeMatch: "/TIL/",
-              },
-              {
-                  text:'TIL',
-                  link:"/TIL/202208/202208",
-                  activeMatch: "/TIL/",
-              },
-              {
-                  text:'회고',
-                  link:"/TIL/202207/20220722",
-                  link:"/TIL/202207/20220729",
-                  activeMatch: "/TIL/",
-              },
+                {
+                    text: 'TIL',
+                    link: "/TIL/202207/202207",
+                    activeMatch: "/TIL/",
+                },
+                {
+                    text: 'TIL',
+                    link: "/TIL/202208/202208",
+                    activeMatch: "/TIL/",
+                },
+                {
+                    text: '회고',
+                    link: "/TIL/202207/20220722",
+                    link: "/TIL/202207/20220729",
+                    activeMatch: "/TIL/",
+                },
             ]
         }
     ]
